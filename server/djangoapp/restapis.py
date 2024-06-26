e0eb229cdb2a12079455db0506b45a6b7737c017
dungnt0810 <dung.nguyen.tien.0810@gmail.com>
2024-06-26 13:22:55 -0400

    module 4

--- server/djangoapp/restapis.py
+++ server/djangoapp/restapis.py
@@ -1,5 +1,5 @@
 # Uncomment the imports below before you add the function code
-# import requests
+import requests
 import os
 from dotenv import load_dotenv
 
@@ -33,6 +33,7 @@ def get_request(endpoint, **kwargs):
 # Add code for retrieving sentiments
 def analyze_review_sentiments(text):
     request_url = sentiment_analyzer_url+"analyze/"+text
+    print(request_url)
     try:
         # Call get method of requests library with URL and parameters
         response = requests.get(request_url)
